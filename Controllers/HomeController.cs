6ad4c0c4f83f6f1a903dcfcfd9568220052b82c5
princess <princ@PrincesQuiatzon22@gmail.com>
2024-10-21 12:07:18 +0800

    added routes for links

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,8 +17,45 @@ public class HomeController : Controller
     {
         return View();
     }
+     public IActionResult Contact()
+    {
+        return View();
+    }
 
+     public IActionResult Event()
+    {
+        return View();
+    }
+
+     public IActionResult Facilities()
+    {
+        return View();
+    }
+     public IActionResult Farmers()
+    {
+        return View();
+    }
+     public IActionResult Gallery()
+    {
+        return View();
+    }
+     public IActionResult Kapihan()
+    {
+        return View();
+    }
     public IActionResult Privacy()
+    {
+        return View();
+    }
+     public IActionResult Review()
+    {
+        return View();
+    }
+     public IActionResult Staff()
+    {
+        return View();
+    }
+     public IActionResult White_beach()
     {
         return View();
     }
